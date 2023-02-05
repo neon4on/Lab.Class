83014994d862d59d339e803469c535246b01405d
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2023-02-05 10:17:19 +0400

    Add files via upload

--- Lab1-5.cpp
+++ Lab1-5.cpp
@@ -1,6 +1,5 @@
 ﻿#include <iostream>
 #include <complex>
-#include <string>
 
 using namespace std;
 
@@ -24,15 +23,20 @@ class Complex {
 		cout << endl;
 	}
 public:
-	Complex(double a1, double b1) { a = a1; b = b1; }
-	~Complex() {
-
+	Complex(double a1, double b1) 
+	{ 
+		a = a1; 
+		b = b1; 
 	}
-	int ComplexSet() {
-
+	~Complex() {
+		cout << endl << "See you soon" << endl;
+	}
+	void ComplexSet(double aa, double bb) {
+		a = aa;
+		b = bb;
 	}
 	void ComplexGet() {
-
+		cout << endl << "\t" << a << " " << b;
 	}
 	void Sum() {
 		Addition();
@@ -58,10 +62,12 @@ int Menu(void)
 		cout << "1 - Сложение" << endl;
 		cout << "2 - Вычитание" << endl;
 		cout << "3 - Умножение" << endl;
-		cout << "4 - Выход из программы" << endl;
+		cout << "4 - Сеттер" << endl;
+		cout << "5 - Геттер" << endl;
+		cout << "6 - Выход из программы" << endl;
 		cout << "ВЫБОР: ";
 		cin >> select;
-	} while ((select < 1) || (select > 4));
+	} while ((select < 1) || (select > 6));
 
 	return select;
 }
@@ -70,6 +76,8 @@ int Menu(void)
 int main(void)
 {
 	setlocale(LC_ALL, "Russian");
+
+	double aa, bb;
 
 	Complex c(10,5);
 
@@ -95,12 +103,26 @@ int main(void)
 			c.Um();
 			break;
 		}
+		case 4:
+		{
+			cout << "Введите первое число: " << endl;
+			cin >> aa;
+			cout << "Введите второе число: " << endl;
+			cin >> bb;
+			c.ComplexSet(aa,bb);
+			break;
 		}
-		if (select != 4)
+		case 5:
+		{
+			c.ComplexGet();
+			break;
+		}
+		}
+		if (select != 6)
 		{
 			cout << endl << endl;
 			system("pause");
 		}
-	} while (select != 4);
+	} while (select != 6);
 	return EXIT_SUCCESS;
 }
